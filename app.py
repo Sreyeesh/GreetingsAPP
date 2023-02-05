e1069f16545dff54335fcd0bf5de51a3876809fb
Sreyeesh <sgarime1@gmail.com>
2023-02-05 02:56:56 +0200

    initial commit fix simplified approach first

--- app.py
+++ app.py
@@ -1,55 +1,25 @@
 import tkinter as tk
-from tkinter import ttk
-from tkinter import messagebox
 import time
+import platform
 
-class GreetApp:
-    def __init__(self, root):
-        self.root = root
-        self.root.title("Greet App")
-        self.root.configure(background='#2C001E')
-        self.root.geometry("800x600")
+def greet_user():
+    name = entry.get()
+    current_time = time.strftime("%H:%M:%S")
+    greet = "Hello " + name + ", it's " + current_time + " here."
+    label.config(text=greet)
 
-        self.label_name = ttk.Label(self.root, text="Name:", font=("TkDefaultFont", 16), foreground='white', background='#2C001E')
-        self.label_name.grid(row=0, column=0, padx=10, pady=10)
+app = tk.Tk()
+app.geometry("300x100")
+app.configure(bg=app.cget("bg"))
+app.title("Greeting App")
 
-        self.entry_name = ttk.Entry(self.root, font=("TkDefaultFont", 16))
-        self.entry_name.grid(row=0, column=1, padx=10, pady=10)
-        self.entry_name.focus()
+label = tk.Label(text="", font=("Arial", 14), bg=app.cget("bg"), fg="black")
+label.pack()
 
-        self.label_location = ttk.Label(self.root, text="Location:", font=("TkDefaultFont", 16), foreground='white', background='#2C001E')
-        self.label_location.grid(row=1, column=0, padx=10, pady=10)
+entry = tk.Entry(font=("Arial", 14), bg="white")
+entry.pack(pady=10)
 
-        self.entry_location = ttk.Entry(self.root, font=("TkDefaultFont", 16))
-        self.entry_location.grid(row=1, column=1, padx=10, pady=10)
+greet_button = tk.Button(text="Greet", font=("Arial", 14), command=greet_user)
+greet_button.pack()
 
-        self.button_greet = ttk.Button(self.root, text="Greet", command=self.greet)
-        self.button_greet.grid(row=2, column=1, pady=10)
-        
-        self.root.bind('<Return>', self.greet)
-
-        self.var_dark_mode = tk.IntVar()
-        self.checkbutton_dark_mode = ttk.Checkbutton(self.root, text="Dark Mode", variable=self.var_dark_mode, command=self.toggle_dark_mode)
-        self.checkbutton_dark_mode.grid(row=3, column=0, columnspan=2, pady=10)
-
-    def greet(self, event=None):
-        name = self.entry_name.get()
-        location = self.entry_location.get()
-        current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-        message = "Hello {}, from {}. Today is {}".format(name, location, current_time)
-        messagebox.showinfo("Greeting", message)
-        
-    def toggle_dark_mode(self):
-        is_dark_mode = self.var_dark_mode.get()
-        if is_dark_mode:
-            self.root.configure(background='#2C001E')
-            self.label_name.configure(foreground='white', background='#2C001E')
-            self.label_location.configure(foreground='white', background='#2C001E')
-        else:
-            self.root.configure(background='white')
-
-
-if __name__ == '__main__':
-    root = tk.Tk()
-    app = GreetApp(root)
-    root.mainloop()
+app.mainloop()
