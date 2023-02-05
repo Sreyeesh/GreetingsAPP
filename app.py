28884b09799b4e941eda64c6392e6da0d3b5b802
Sreyeesh <sgarime1@gmail.com>
2023-02-05 12:30:17 +0200

    updated code with grid geomotery, put into a class. Added in capability to hit enter and it returns an error when hitting enter on keyboard

--- app.py
+++ app.py
@@ -1,25 +1,47 @@
 import tkinter as tk
+from tkinter import messagebox
 import time
-import platform
+import geocoder
 
-def greet_user():
-    name = entry.get()
-    current_time = time.strftime("%H:%M:%S")
-    greet = "Hello " + name + ", it's " + current_time + " here."
-    label.config(text=greet)
+class GreetingApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Greeting App")
 
-app = tk.Tk()
-app.geometry("300x100")
-app.configure(bg=app.cget("bg"))
-app.title("Greeting App")
+        self.entry_frame = tk.Frame(self.root)
+        self.entry_frame.grid(row=0, column=0, padx=10, pady=10)
 
-label = tk.Label(text="", font=("Arial", 14), bg=app.cget("bg"), fg="black")
-label.pack()
+        self.entry_label = tk.Label(self.entry_frame, text="Insert name:", font=("Arial", 14))
+        self.entry_label.grid(row=0, column=0)
 
-entry = tk.Entry(font=("Arial", 14), bg="white")
-entry.pack(pady=10)
+        self.name_entry = tk.Entry(self.entry_frame, font=("Arial", 14), width=20)
+        self.name_entry.grid(row=0, column=1)
+        self.name_entry.focus()
+        self.name_entry.bind("<Return>", self.greet_user)
 
-greet_button = tk.Button(text="Greet", font=("Arial", 14), command=greet_user)
-greet_button.pack()
+        self.button_frame = tk.Frame(self.root)
+        self.button_frame.grid(row=1, column=0, padx=10, pady=10)
 
-app.mainloop()
+        self.greet_button = tk.Button(self.button_frame, text="Greet", font=("Arial", 14), command=self.greet_user)
+        self.greet_button.grid(row=0, column=0, padx=10, pady=10)
+
+        self.quit_button = tk.Button(self.button_frame, text="Quit", font=("Arial", 14), command=self.root.quit)
+        self.quit_button.grid(row=0, column=1, padx=10, pady=10)
+
+    def greet_user(self, event=None):
+        name = self.name_entry.get().strip()
+        try:
+            if not name:
+                raise ValueError("Please enter your name to continue")
+            g = geocoder.ip('me')
+            location = g.city + ", " + g.state
+            current_time = time.strftime("%I:%M %p")
+            greet = f"Hello {name}, it is currently {current_time} in {location}"
+            messagebox.showinfo("Greeting", greet)
+        except Exception as error:
+            messagebox.showerror("Error", str(error))
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = GreetingApp(root)
+    root.mainloop()
